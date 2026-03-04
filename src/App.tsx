d301e01f69b38207789326087855edc10b1507f8
gzavertyaev-dev <gzavertyaev.dev@gmail.com>
2026-03-04 21:11:17 +0100

    Add frontend lesson 11

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,14 @@ import Homework_06 from "./homeworks/Homework_06/Homework_06";
 import Homewwork_07 from "homeworks/Homework_07/Homework_07";
 import Homework_08 from "homeworks/Homework_08/Homework_08";
 import Homework_09 from "homeworks/Homework_09/Homework_09";
+import Homework_10 from "homeworks/Homework_10/Homework_10";
 
 // Lessons
 import Lesson_07 from "./lessons/Lesson_07/Lesson_07";
 import Lesson_06 from "./lessons/Lesson_06/Lesson_06";
 import Lesson_08 from "lessons/Lesson_08/Lesson_08";
 import Lesson_09 from "lessons/Lesson_09/Lesson_09";
-import Lesson_10 from "lessons/Lesson_10/Lesson_10";
+// import Lesson_10 from "lessons/Lesson_10/Lesson_10";
 
 function App() {
   return (
@@ -21,12 +22,13 @@ function App() {
       {/* <Homework_06 /> */}
       {/* <Homewwork_07 /> */}
       {/* <Homework_09 /> */}
+      <Homework_10 />
       {/* Lessons */}
       {/* <Lesson_06 /> */}
       {/* <Lesson_07 /> */}
       {/* <Lesson_08 /> */}
       {/* <Lesson_09 /> */}
-      <Lesson_10 />
+      {/* <Lesson_10 /> */}
     </>
   );
 }
