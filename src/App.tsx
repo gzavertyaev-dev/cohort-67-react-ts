0185fc539c02797c6fa665ba6aa9918302cf9ad9
gzavertyaev-dev <gzavertyaev.dev@gmail.com>
2026-03-11 22:06:24 +0100

    Add lesson 13

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,12 @@
+// Импортируем BrowserRouter, Route, Routes для настройки маршрутизации
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Layout from "components/Layout/Layout";
 import GlobalStyles from "styles/GlobalStyles";
+import Home from "pages/EmployeeApp/Home/Home";
+import About from "pages/EmployeeApp/About/About";
+import LogIn from "pages/EmployeeApp/LogIn/LogIn";
+import ContactUs from "pages/EmployeeApp/ContactUs/ContactUs";
 
 // Homeworks
 import Homework_06 from "./homeworks/Homework_06/Homework_06";
@@ -6,6 +14,7 @@ import Homewwork_07 from "homeworks/Homework_07/Homework_07";
 import Homework_08 from "homeworks/Homework_08/Homework_08";
 import Homework_09 from "homeworks/Homework_09/Homework_09";
 import Homework_10 from "homeworks/Homework_10/Homework_10";
+import Homework_12 from "homeworks/Homework_12/Homework_12";
 
 // Lessons
 import Lesson_07 from "./lessons/Lesson_07/Lesson_07";
@@ -17,21 +26,31 @@ import Lesson_12 from "lessons/Lesson_12/Lesson_12";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <GlobalStyles />
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/contactUs" element={<ContactUs />} />
+          <Route path="*" element="Page Not Found" />
+        </Routes>
+      </Layout>
       {/* Homeworks */}
       {/* <Homework_06 /> */}
       {/* <Homewwork_07 /> */}
       {/* <Homework_09 /> */}
       {/* <Homework_10 /> */}
+      {/* <Homework_12 /> */}
       {/* Lessons */}
       {/* <Lesson_06 /> */}
       {/* <Lesson_07 /> */}
       {/* <Lesson_08 /> */}
       {/* <Lesson_09 /> */}
       {/* <Lesson_10 /> */}
-      <Lesson_12 />
-    </>
+      {/* <Lesson_12 /> */}
+    </BrowserRouter>
   );
 }
 
